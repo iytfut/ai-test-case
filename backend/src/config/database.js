49c358d5f33ff554e55ac7b418a282f8f3701804
git senpai <satyamshukla.dev@gmail.com>
2025-09-13 11:30:46 +0530

    fixed api error

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -36,10 +36,7 @@ export const config = {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
-        ? [
-            process.env.FRONTEND_URL,
-            "https://your-frontend-app-name.vercel.app",
-          ]
+        ? [process.env.FRONTEND_URL, "https://ai-test-case-nu.vercel.app/"]
         : process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
